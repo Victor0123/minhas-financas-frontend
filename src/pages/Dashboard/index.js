56fe519b9b31ff27ac42d88a934835a34fc521fa
Victor André Rossini de Souza <victor.rossini01@hotmail.com>
2021-01-20 16:37:54 -0300

    Exibindo dados da api

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import {
+  Box,
+  Container,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+  ButtonGroup,
+  Chip,
+  List,
+  ListItem,
+  ListItemText,
+} from '@material-ui/core';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-import { Chip, List, ListItem, ListItemText } from '@material-ui/core';
 import Menu from '../../components/menu';
 import Footer from '../../components/footer';
 import api from '../../services/api';
@@ -39,11 +48,19 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     overflow: 'auto',
     flexDirection: 'column',
+    marginBottom: 20,
   },
   list: {
     width: '100%',
-    // maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
+  },
+  button: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
   },
 }));
 
@@ -52,15 +69,19 @@ export default function Dashboard() {
 
   const [lancamentos, setLancamentos] = useState([]);
   const [totalizadores, setTotalizadores] = useState([]);
+  const [year, setYear] = useState(new Date());
+  const [selectMonth, setSelectMonth] = useState('01');
 
   useEffect(() => {
     async function loadLancamentos() {
-      const response = await api.get('lancamentos/2020-06');
+      const response = await api.get(
+        `lancamentos/${year.getFullYear()}-${selectMonth}`,
+      );
       setLancamentos(response.data.Lancamentos);
       setTotalizadores(response.data.Totalizadores);
     }
     loadLancamentos();
-  }, []);
+  }, [selectMonth, year]);
 
   return (
     <div className={classes.root}>
@@ -68,7 +89,7 @@ export default function Dashboard() {
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={2}>
+          <Grid container spacing={3}>
             <Grid>
               <Paper className={classes.paper}>
                 <h3>Totalizadores</h3>
@@ -77,18 +98,62 @@ export default function Dashboard() {
                     <ListItem key={totalizador.id}>
                       <ListItemText
                         primary={totalizador.conta}
-                        secondary={parseFloat(totalizador.valor).toLocaleString(
-                          'pt-br',
-                          {
-                            style: 'currency',
-                            currency: 'BRL',
-                          },
-                        )}
+                        secondary={
+                          totalizador.valor > 0 ? (
+                            <Chip
+                              label={parseFloat(
+                                totalizador.valor,
+                              ).toLocaleString('pt-br', {
+                                style: 'currency',
+                                currency: 'BRL',
+                              })}
+                              color="primary"
+                            />
+                          ) : (
+                            <Chip
+                              label={parseFloat(
+                                totalizador.valor,
+                              ).toLocaleString('pt-br', {
+                                style: 'currency',
+                                currency: 'BRL',
+                              })}
+                              color="secondary"
+                            />
+                          )
+                        }
                       />
                     </ListItem>
                   ))}
                 </List>
               </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <DatePicker
+                selected={year}
+                onChange={date => setYear(date)}
+                showYearPicker
+                dateFormat="yyyy"
+              />
+              <div className={classes.button}>
+                <ButtonGroup
+                  variant="contained"
+                  color="primary"
+                  aria-label="contained primary button group"
+                >
+                  <Button onClick={() => setSelectMonth('01')}>Jan</Button>
+                  <Button onClick={() => setSelectMonth('02')}>Fev</Button>
+                  <Button onClick={() => setSelectMonth('03')}>Mar</Button>
+                  <Button onClick={() => setSelectMonth('04')}>Abr</Button>
+                  <Button onClick={() => setSelectMonth('05')}>Mai</Button>
+                  <Button onClick={() => setSelectMonth('06')}>Jun</Button>
+                  <Button onClick={() => setSelectMonth('07')}>Jul</Button>
+                  <Button onClick={() => setSelectMonth('08')}>Ago</Button>
+                  <Button onClick={() => setSelectMonth('09')}>Set</Button>
+                  <Button onClick={() => setSelectMonth('10')}>Out</Button>
+                  <Button onClick={() => setSelectMonth('11')}>Nov</Button>
+                  <Button onClick={() => setSelectMonth('12')}>Dez</Button>
+                </ButtonGroup>
+              </div>
             </Grid>
             <Grid item xs={12}>
               <TableContainer component={Paper}>
