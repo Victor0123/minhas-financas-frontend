e4e7c22ec0e5421afa472dca0b1d9f5aa5aeff99
Victor André Rossini de Souza <victor.rossini01@hotmail.com>
2021-02-13 19:33:53 -0300

    Logout da aplicaçao

--- src/components/Menu/listMenu.js
+++ src/components/Menu/listMenu.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import {
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  ListSubheader,
-} from '@material-ui/core';
+import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import PersonIcon from '@material-ui/icons/Person';
-import ExitToApp from '@material-ui/icons/ExitToApp';
 
 export const mainListItems = (
   <div>
@@ -22,18 +16,6 @@ export const mainListItems = (
         <PersonIcon />
       </ListItemIcon>
       <ListItemText primary="Meu perfil" />
-    </ListItem>
-  </div>
-);
-
-export const secondaryListItems = (
-  <div>
-    <ListSubheader inset>opções</ListSubheader>
-    <ListItem button component="a" href="/">
-      <ListItemIcon>
-        <ExitToApp />
-      </ListItemIcon>
-      <ListItemText primary="Sair" />
     </ListItem>
   </div>
 );
